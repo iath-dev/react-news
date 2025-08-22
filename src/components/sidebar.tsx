b80b5f4229f434872d26b4b193d3f302e076e60b
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-08-22 08:57:46 -0500

    feat: app title and overlay for the sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -9,65 +9,74 @@ const Sidebar: React.FC = () => {
   const { isOpen, toggle } = useNavigationStore()
 
   return (
-    <aside
-      className={cn(
-        'row-span-2 p-4 flex flex-col h-screen gap-4 border-r border-base-300 shadow-xl sticky z-20 top-0 max-md:absolute bg-base-100 w-[min(16rem,100%)] transition-transform duration-500',
-        {
-          'max-md:-translate-x-full': !isOpen,
-          'left-0': isOpen,
-        }
-      )}
-    >
-      <div className="flex gap-3.5 items-center">
-        <img src={logo} alt="logo" className="dark:invert size-[2em]" />
-        <h1 className="text-xl flex-1">
-          React <span className="text-accent">News</span>
-        </h1>
-        <button
-          aria-label="collapse button"
-          onClick={() => toggle()}
-          className="btn btn-square btn-outline btn-sm btn-ghost md:hidden"
-        >
-          <ArrowLeftToLine
-            className={cn('size-[1em] transition-transform duration-500', {
-              'rotate-180': !isOpen,
-            })}
-          />
-        </button>
-      </div>
-      <ul className="space-y-2 overflow-y-auto">
-        <li>
-          <a
-            href="/"
-            className={cn(
-              'btn btn-md btn-ghost hover:btn-soft hover:btn-primary w-full justify-start'
-            )}
+    <>
+      <aside
+        className={cn(
+          'row-span-2 p-4 flex flex-col h-screen gap-4 border-r border-base-300 shadow-xl sticky z-20 top-0 max-md:absolute bg-base-100 w-[min(16rem,100%)] transition-transform duration-500',
+          {
+            'max-md:-translate-x-full': !isOpen,
+            'left-0': isOpen,
+          }
+        )}
+      >
+        <div className="flex gap-3.5 items-center">
+          <img src={logo} alt="logo" className="dark:invert size-[2em]" />
+          <h1 className="text-xl flex-1">
+            React <span className="text-accent">News</span>
+          </h1>
+          <button
+            aria-label="collapse button"
+            onClick={() => toggle()}
+            className="btn btn-square btn-outline btn-sm btn-ghost md:hidden"
           >
-            <Search className="size-[1em]" />
-            <span className="first-letter:uppercase">Search</span>
-          </a>
-        </li>
-        <li className="text-sm font-semibold text-base-content/75 my-2.5 ml-2">
-          Categories
-        </li>
-        {NAV_ITEMS.map(({ category, icon: Icon }) => (
-          <li key={category}>
-            <NavLink
-              to={`/${category}`}
-              className={({ isActive }) =>
-                cn(
-                  'btn btn-md hover:btn-soft hover:btn-primary w-full justify-start',
-                  isActive ? 'btn-primary' : 'btn-ghost'
-                )
-              }
+            <ArrowLeftToLine
+              className={cn('size-[1em] transition-transform duration-500', {
+                'rotate-180': !isOpen,
+              })}
+            />
+          </button>
+        </div>
+        <ul className="space-y-2 overflow-y-auto">
+          <li>
+            <a
+              href="/"
+              className={cn(
+                'btn btn-md btn-ghost hover:btn-soft hover:btn-primary w-full justify-start'
+              )}
             >
-              <Icon className="size-[1em]" />
-              <span className="first-letter:uppercase">{category}</span>
-            </NavLink>
+              <Search className="size-[1em]" />
+              <span className="first-letter:uppercase">Search</span>
+            </a>
           </li>
-        ))}
-      </ul>
-    </aside>
+          <li className="text-sm font-semibold text-base-content/75 my-2.5 ml-2">
+            Categories
+          </li>
+          {NAV_ITEMS.map(({ category, icon: Icon }) => (
+            <li key={category}>
+              <NavLink
+                to={`/${category}`}
+                className={({ isActive }) =>
+                  cn(
+                    'btn btn-md hover:btn-soft hover:btn-primary w-full justify-start',
+                    isActive ? 'btn-primary' : 'btn-ghost'
+                  )
+                }
+              >
+                <Icon className="size-[1em]" />
+                <span className="first-letter:uppercase">{category}</span>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </aside>
+      <div
+        className={cn(
+          'fixed inset-0 z-[15] bg-base-300/50',
+          isOpen ? 'max-md:flex hidden' : 'hidden'
+        )}
+        onClick={toggle}
+      ></div>
+    </>
   )
 }
 
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,22 +2,31 @@ import Sidebar from '@/components/sidebar'
 import { useNavigationStore } from '@/hooks/useNavigationStore'
 import { MenuIcon } from 'lucide-react'
 import type React from 'react'
-import { Outlet } from 'react-router'
+import { Outlet, useParams } from 'react-router'
 
 const RootLayout: React.FC = () => {
   const { toggle } = useNavigationStore()
+  const { category } = useParams()
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-[16rem_1fr] grid-rows-[auto_1fr] h-screen w-screen">
       <Sidebar />
       <header className="p-4 flex items-center justify-between sticky top-0 bg-base-200 z-10">
-        <button
-          aria-label="toggle sidebar"
-          className="btn btn-ghost btn-square max-md:flex hidden"
-          onClick={() => toggle()}
-        >
-          <MenuIcon className="size-[1em]" />
-        </button>
+        <div className="max-md:flex hidden gap-3.5 items-center">
+          <button
+            aria-label="toggle sidebar"
+            className="btn btn-ghost btn-square"
+            onClick={() => toggle()}
+          >
+            <MenuIcon className="size-[1em]" />
+          </button>
+          <h1 className="text-xl flex-1">
+            React <span className="text-accent">News</span>{' '}
+            {category && (
+              <span className="capitalize text-gray-600"> ~ {category}</span>
+            )}
+          </h1>
+        </div>
       </header>
       <main className="container mx-auto p-4 overflow-y-auto overflow-x-hidden">
         <Outlet />
